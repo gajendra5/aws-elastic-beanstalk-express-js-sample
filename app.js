c04d20e09f969b17ae68372459e0aa540cba4fe8
tester1 <srishiv.govind@gmail.com>
2021-04-30 08:29:47 -0400

    Third Revision of display message

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome To AWS Cloud Computing!!'));  
-app.get('/', (req, res) => res.send('==>> Develop/Maintain/Operate Your Computing Environment From AnyWhere in the World!'));
+app.get('/', (req, res) => res.send('=>> Develop/Maintain/Operate <<<==  Your Computing Environment From AnyWhere in the World!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
